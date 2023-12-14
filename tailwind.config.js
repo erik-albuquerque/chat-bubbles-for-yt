14240977dbaa9186fb7747abeec991248d20c606
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-12-14 14:22:24 -0300

    feat: add chroma key background

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          500: '#00e600'
+        }
+      }
+    },
   },
   plugins: [],
 }
