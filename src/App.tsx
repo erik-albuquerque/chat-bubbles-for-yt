a09cc9eb44df1bdf36aaef1918a6eafe474e3ba0
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-12-14 15:41:00 -0300

    feat: show message after type "Enter"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
+import { useCallback, useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
+
+type Message = {
+  id: string;
+  content?: string;
+};
+
 function App() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [message, setMessage] = useState<Message>({ id: "", content: "" });
+
+  const onTyping = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.code !== "Enter") {
+        setMessage((prevMessage) => ({
+          id: uuid(),
+          content: prevMessage.content + e.key,
+        }));
+
+      } else if (message.content?.trim() !== "") {
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          {
+            id: uuid(),
+            content: message.content,
+          },
+        ]);
+
+        setMessage({ id: "", content: "" });
+      }
+    },
+    [message.content]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", onTyping);
+
+    return () => window.removeEventListener("keydown", onTyping);
+  }, [onTyping]);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main className="w-screen h-screen flex items-end">
+      <div className="pl-4 pb-16 flex flex-col gap-2">
+        {messages.map((message) => (
+          <div
+            key={message.id}
+            className="w-full max-w-fit bg-white rounded-full pl-3 pr-4 py-2 relative"
+          >
+            <div className="w-6 h-6 bg-white absolute left-0 bottom-0 -z-10 rounded-sm" />
+            <span className="font-medium text-base">{message.content}</span>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/cn.ts
@@ -0,0 +1,9 @@
+import clsx, { ClassValue } from "clsx";
+
+import { twMerge } from "tailwind-merge";
+
+const cn = (...args: ClassValue[]) => {
+  return twMerge(clsx(...args));
+};
+
+export { cn };
